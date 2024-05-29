2e45d58b968ce49e7efd0023e3ab3592a34b2fbd
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-05-29 15:01:19 +0200

    ADD: ResourceController index method

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Comic;
 
 use Illuminate\Http\Request;
 
 class ResourceController extends Controller
 {
-    //
+    public function index() {
+
+        $comics = Comic::all();
+
+        return view('comics.index',compact ('comics'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResourceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+//comics index
+Route::get('/comics', [ResourceController::class, 'index']);
